d16cef2e90ce77ad32d7beca022af36d4833a5bc
unico <ramezan.mahmoodi.arshia@gmail.com>
2024-09-09 21:49:21 +0200

    add bash script test for installation

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#add the the command line allias to run the project for zsh terminals
+echo 'alias do="/home/arshia/Dev/simple_todolist/DO/target/debug/DO"' >> ~/.zshrc
+source ~/.zshrc
