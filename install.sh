a94fcf2499e0d2eeb953c824dcc01359432ab903
unico <ramezan.mahmoodi.arshia@gmail.com>
2024-09-30 10:57:46 +0200

    install script path fixed

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 #add the the command line allias to run the project for zsh terminals
-echo 'alias do="/home/arshia/Dev/simple_todolist/DO/target/debug/DO"' >> ~/.zshrc
+echo 'alias do="$PWD/target/debug/DO"' >> ~/.zshrc
 source ~/.zshrc
